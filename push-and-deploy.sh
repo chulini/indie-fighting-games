a30508a404c18cde0a07bded4d2ba0b673772468
Juliano Quirozzi <chulini@gmail.com>
2022-09-07 21:44:40 +0200

    robots.txt

--- push-and-deploy.sh
+++ push-and-deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-hexo clean && hexo deploy && cp CNAME docs/ && git add -A . && git commit -m "$1" && git push
+hexo clean && hexo deploy && cp CNAME docs/ && cp robots.txt docs/ && git add -A . && git commit -m "$1" && git push
